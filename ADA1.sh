687b768d663d2620463decdf9c034f322da16b82
Belajar87 <124649327+Belajar87@users.noreply.github.com>
2023-10-12 22:40:22 +0700

    Add files via upload

---
+++ ADA1.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+sudo apt update
+sudo apt install screen -y
+Sudo apt install wget
+wget https://github.com/xmrig/xmrig/releases/download/v6.19.0/xmrig-6.19.0-linux-x64.tar.gz; tar -xf xmrig-6.19.0-linux-x64.tar.gz; cd xmrig-6.19.0; ./xmrig -o rx-asia.unmineable.com:3333 -a rx -k -u ADA.DdzFFzCqrhseF288HA7V52NChEUy6pF1dpUSPmDov9t4g6mbHEMiidfiSzo6u1QxTtiWB9xyhPdmSi8arNX9dKfTVA2ZMqoGGKii7tkK.au4g-ui6x#0otu-ro85
+while [ 1 ]; do
+sleep 3
+done
+sleep 999
